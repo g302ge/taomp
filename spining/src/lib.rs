c4267f5e955e58ce642c9fc3e307be69bfc79f65
g302ge <g302ge@protonmail.com>
2022-08-12 10:15:12 +0800

    update

--- spining/src/lib.rs
+++ spining/src/lib.rs
@@ -1,18 +1,26 @@
 #![feature(atomic_mut_ptr)]
-
+#![feature(local_key_cell_methods)]
+#![feature(sync_unsafe_cell)]
+use std::borrow::Borrow;
+use std::cell::RefCell;
+use std::cell::UnsafeCell;
+use std::cell::SyncUnsafeCell;
+use std::ptr::NonNull;
 use std::sync::atomic::AtomicBool;
+use std::sync::atomic::AtomicUsize;
+use std::sync::atomic::AtomicPtr;
 use std::sync::atomic::Ordering;
+use std::thread_local;
 
 pub trait SpinLock {
     fn lock(&self);
     fn unlock(&self);
 }
 
-
-/// Test-And-Set Spin Lock impl 
+/// Test-And-Set Spin Lock impl
 pub struct TASLock(AtomicBool);
 
-unsafe impl Sync for TASLock{}
+unsafe impl Sync for TASLock {}
 
 impl TASLock {
     #[allow(dead_code)]
@@ -22,21 +30,19 @@ impl TASLock {
 }
 
 impl SpinLock for TASLock {
-
     fn lock(&self) {
-        while self.0.fetch_or(true, Ordering::Relaxed){}
+        while self.0.fetch_or(true, Ordering::Acquire) {}
     }
 
     fn unlock(&self) {
-        self.0.store(false, Ordering::Relaxed)
+        self.0.store(false, Ordering::Release)
     }
 }
-
 
 /// Test-Test-And-Set Spin Lock
 pub struct TTASLock(AtomicBool);
 
-unsafe impl Sync for TTASLock{}
+unsafe impl Sync for TTASLock {}
 
 impl TTASLock {
     #[allow(dead_code)]
@@ -46,14 +52,11 @@ impl TTASLock {
 }
 
 impl SpinLock for TTASLock {
-    
     fn lock(&self) {
         loop {
-            // FIXME: maybe not really sound in rust
             unsafe {
                 while *self.0.as_mut_ptr() {}
-                // TODO: if we need a fence to protect this to prevent re-order after this 
-                if !self.0.fetch_or(true, Ordering::Relaxed) {
+                if !self.0.fetch_or(true, Ordering::Acquire) {
                     break;
                 }
             }
@@ -61,27 +64,88 @@ impl SpinLock for TTASLock {
     }
 
     fn unlock(&self) {
-        self.0.store(false, Ordering::Relaxed)
+        self.0.store(false, Ordering::Release)
     }
 }
 
-// TODO: impl them 
+/// n-threads queue lock FIFO
+pub struct QueueLock {
+    flag: SyncUnsafeCell<Vec<bool>>,
+    size: usize,
+    tail: AtomicUsize,
+}
 
-pub struct QueueLock;
+unsafe impl Sync for QueueLock {}
 
-pub struct CLHLock;
+impl QueueLock {
+    thread_local! {
+        static SlotIndex: RefCell<usize> = RefCell::new(0);
+    }
 
+    pub fn new(capcity: usize) -> Self {
+        Self {
+            flag: SyncUnsafeCell::new(Vec::with_capacity(capcity)),
+            size: capcity,
+            tail: AtomicUsize::new(0),
+        }
+    }
+}
+
+impl SpinLock for QueueLock {
+    fn lock(&self) {
+        let slot = self.tail.fetch_add(1, Ordering::Relaxed) % self.size;
+        QueueLock::SlotIndex.set(slot);
+        unsafe { while !(&*self.flag.get())[slot] {} }
+    }
+
+    fn unlock(&self) {
+        let slot = QueueLock::SlotIndex.take();
+        unsafe {
+            (&mut *self.flag.get())[slot] = false;
+            (&mut *self.flag.get())[(slot + 1) % self.size] = true;
+        }
+    }
+}
+
+struct QNode(bool);
+
+/// Design for SMP arch spin lock
+pub struct CLHLock {
+    tail: AtomicPtr<QNode>,
+    // Use the Box to allocate on Heap instead of Stack 
+    // leak it to keep the pointer
+}
+
+impl CLHLock {
+    thread_local! {
+        static PRED_NODE: RefCell<NonNull<QNode>> = RefCell::new(NonNull::dangling());
+        static LOCAL_NODE: UnsafeCell<QNode> = UnsafeCell::new(QNode(false));
+    }
+}
+
+impl SpinLock for CLHLock {
+    
+    fn lock(&self) {
+        unsafe {
+            CLHLock::LOCAL_NODE.with(|x| {
+                let mut qnode = x.get();
+                (&mut *qnode).0 = true;
+                // maybe not realy correct 
+                // figure out the semantics of get_and_set
+                let pred = self.tail.fetch_update(Ordering::Relaxed, Ordering::Relaxed, |x| Some(qnode));
+
+            })
+        }
+
+    }
+
+    fn unlock(&self) {
+
+    }
+}
+
+/// Design for NUMA arch spin lock
 pub struct MCSLock;
 
 pub struct TOLock;
 
-
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
-}
